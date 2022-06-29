51fe4cc1f8cf679c8c7c07eaf1d80246e9d9b3cb
gamejunkiez <queenangrylobster@gmail.com>
2022-06-29 09:16:29 -0500

    completed exercise

---
+++ BasicClassesExerfcise/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExerfcise
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"The vehicle is a {Make} {Model} produced in {Year}.");
+        }
+    }
+}
---
+++ BasicClassesExerfcise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExerfcise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ford = new Car();
+            ford.Make = "Ford";
+            ford.Model = "F150";
+            ford.Year = 1999;
+            ford.PrintInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
